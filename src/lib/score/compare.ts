af7ad2bbbb87beb2f59597d7238f7e0ebd4b5029
Front-line-dev <kimminsung@kakao.com>
2023-04-06 22:13:39 +0900

    채점 알고리즘 복합 적용

--- src/lib/score/compare.ts
+++ src/lib/score/compare.ts
@@ -1,15 +1,44 @@
 import { toPixelData } from 'html-to-image';
 
-export default async function compare(containerA, containerB): number {
+export default async function compare(containerA, containerB) {
   const userPixels = await toPixelData(containerA);
   const answerPixels = await toPixelData(containerB);
 
-  // compare canvas
-  const pixelLength = userPixels.length;
-  let identicalPixels = 0;
-  for (let i = 0; i < pixelLength; i++) {
-    if (userPixels[i] === answerPixels[i]) identicalPixels++;
+  if (userPixels.length !== answerPixels.length) {
+    console.error('Two canvas sizes are not identical');
   }
 
-  return (identicalPixels / pixelLength) * 100;
+  // compare canvas
+  const scoreSpectrum = calcSpectrum(userPixels, answerPixels);
+  const scorePerfect = calcPixelPerfect(userPixels, answerPixels);
+  console.log(scoreSpectrum, scorePerfect);
+
+  return scoreSpectrum * scorePerfect * 100;
+}
+
+function calcSpectrum(userPixels, answerPixels) {
+  const pixelLength = userPixels.length;
+  const spectrum = new Array(256).fill(0);
+
+  // 데스크톱에서 약 3ms 계산
+  for (let i = 0; i < pixelLength; i += 1) {
+    spectrum[userPixels[i]] += 1;
+    spectrum[answerPixels[i]] -= 1;
+  }
+
+  const MAX_DIFF = pixelLength / 4;
+  const difference = spectrum.reduce((acc, cur) => acc + Math.abs(cur), 0);
+  return 1 - difference / MAX_DIFF;
+}
+
+function calcPixelPerfect(userPixels, answerPixels) {
+  const pixelLength = userPixels.length;
+  let identicalPixels = 0;
+  for (let i = 0; i < pixelLength; i += 1) {
+    if (userPixels[i] === answerPixels[i]) {
+      identicalPixels += 1;
+    }
+  }
+
+  return identicalPixels / pixelLength;
 }
