3d9493a7c40d33471bfe9979e1ee0efb1e51d604
sangwonlee <sangwonlee@nts-corp.com>
2023-04-09 14:02:00 +0900

    퀴즈페이지, 헤더

---
+++ src/component/header/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import styles from "./header.module.scss";
+
+function Header() {
+  const [menuArea, setMenuArea] = useState(false)
+  const [toastPopup, setToastPopup] = useState(false)
+  const openMenu = () => {
+    setMenuArea(!menuArea)
+    if (menuArea)
+      document.body.style.overflow = 'hidden'
+    else
+      document.body.style.overflow = 'auto'
+  }
+
+  const copyUrl = () => {
+    navigator.clipboard.writeText(window.location.href)
+    setToastPopup(true)
+    const timer = setTimeout(() => {
+      setToastPopup(false);
+    }, 1500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.title}>
+        <a className={styles.home_link} href="/">
+          Can you markup?
+        </a>
+      </h1>
+      <div className={styles.button_area}>
+        <button type="button" className={styles.button} onClick={copyUrl}>
+          <span className="blind">공유</span>
+        </button>
+        <button type="button" onClick={() => openMenu()} className={styles.button}>
+          dd
+        </button>
+        {menuArea &&
+          <div className={styles.dimmed} onClick={() => openMenu()}>
+            <div className={styles.menu_area}>dd</div>
+          </div>
+        }
+        {toastPopup &&
+          <p className={styles.share_text}>복사 되었습니다</p>
+        }
+      </div>
+    </header>
+  )
+}
+export default Header
---
+++ src/pages/quiz.tsx
@@ -0,0 +1,27 @@
+// import Header from '../component/header'
+import Header from '@component/header'
+import styles from "./quiz.module.scss";
+
+export default function Index() {
+  // const [menuArea, setMenuArea] = useState(false)
+  // const [htmlOpen, setHtmlOpen] = useState(true)
+  // const [cssOpen, SetCssOpen] = useState(false)
+  // const openHtml = () => {
+  //   setHtmlOpen(true)
+  //   SetCssOpen(false)
+  // }
+  // const openCss = () => {
+  //   setHtmlOpen(false)
+  //   SetCssOpen(true)
+  // }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <div>dd</div>
+      </main>
+      <p className={styles.a}>d</p>
+    </>
+  );
+}
