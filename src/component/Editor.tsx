34e0b17b069c61235ec7e52ec1de38f6e9cd9f2b
Front-line-dev <kimminsung@kakao.com>
2023-05-09 15:28:25 +0900

    에디터 컴포넌트화

---
+++ src/component/Editor.tsx
@@ -0,0 +1,15 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { okaidia } from '@uiw/codemirror-theme-okaidia';
+import { html } from '@codemirror/lang-html';
+import { css } from '@codemirror/lang-css';
+import wrapDebounce from '@lib/utils';
+
+const LANG_MAP = {
+  html: [html({ autoCloseTags: true })],
+  css: [css()],
+};
+
+export default function Editor({ lang, initialString, setState }: { lang: string; initialString: string; setState: Dispatch<SetStateAction<string>> }) {
+  return <CodeMirror value={initialString} theme={okaidia} width="380px" height="380px" extensions={LANG_MAP[lang]} onChange={wrapDebounce(setState, 1000)} />;
+}
