aaca10ef7bd7a2aa1a221a7e1065dcf7983dcbdf
Synagir <lee3708013@gmail.com>
2023-05-20 22:41:02 +0900

    리뷰 반영

--- src/pages/test1.tsx
+++ src/pages/test1.tsx
@@ -7,35 +7,47 @@ import { html } from '@codemirror/lang-html';
 import { css } from '@codemirror/lang-css';
 
 const router = 'test1';
+
 export default function Test1() {
-  const dataBaseItem = useLiveQuery(() => db.markups.where('id').equals(router).toArray());
+  const dataBaseItem = useLiveQuery(() => db.markups.where('id').equals(router).toArray())?.shift();
   const [htmlState, setHtmlState] = useState('');
   const [cssState, setCssState] = useState('');
+
+  const addState = async () => {
+    try {
+      await db.markups.add({ id: router, htmlState, cssState }, router);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateState = async (view, viewUpdate) => {
+    try {
+      const existedData = (await db.markups.where('id').equals(router).toArray()).length;
+      if (!existedData) addState();
+
+      const stateType = viewUpdate.view.contentAttrs['data-language'];
+
+      if (stateType === 'html') {
+        await db.markups.update(router, { htmlState: view });
+      } else if (stateType === 'css') {
+        await db.markups.update(router, { cssState: view });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
-    setHtmlState(dataBaseItem?.at(0)?.htmlState);
-    setCssState(dataBaseItem?.at(0)?.cssState);
+    if (dataBaseItem) {
+      setHtmlState(dataBaseItem?.htmlState);
+      setCssState(dataBaseItem?.cssState);
+    }
   }, [dataBaseItem]);
-
-  const updateHtmlState = async (view) => {
-    try {
-      await db.markups.put({ id: router, htmlState: view, cssState }, router);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const updateCssState = async (view) => {
-    try {
-      await db.markups.put({ id: router, htmlState, cssState: view }, router);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return (
     <div>
-      <CodeMirror value={htmlState} theme={okaidia} width="380px" height="380px" extensions={[html({ autoCloseTags: true })]} onChange={updateHtmlState} />
-      <CodeMirror value={cssState} theme={okaidia} width="380px" height="380px" extensions={[css()]} onChange={updateCssState} />
+      <CodeMirror value={htmlState} theme={okaidia} width="380px" height="380px" extensions={[html({ autoCloseTags: true })]} onChange={updateState} />
+      <CodeMirror value={cssState} theme={okaidia} width="380px" height="380px" extensions={[css()]} onChange={updateState} />
     </div>
   );
 }
