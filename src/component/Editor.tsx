4b11fdc7fb19421795704d9242857e5553d13869
Synagir <lee3708013@gmail.com>
2023-05-28 21:01:15 +0900

    코드 에디터 높이 고정, 캔버스 최대 크기 제한

--- src/component/Editor.tsx
+++ src/component/Editor.tsx
@@ -11,15 +11,5 @@ const LANG_MAP = {
 };
 
 export default function Editor({ lang, initialString, setState }: { lang: string; initialString: string; setState: Dispatch<SetStateAction<string>> }) {
-  return (
-    <CodeMirror
-      value={initialString}
-      theme={okaidia}
-      width="100%"
-      height="100%"
-      maxHeight="380px"
-      extensions={LANG_MAP[lang]}
-      onChange={wrapDebounce(setState, 1000)}
-    />
-  );
+  return <CodeMirror value={initialString} theme={okaidia} width="100%" height="380px" extensions={LANG_MAP[lang]} onChange={wrapDebounce(setState, 1000)} />;
 }
